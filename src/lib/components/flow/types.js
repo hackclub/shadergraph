ef09374ea608b9c22c42befe59bf298dffedc429
Malted <malted@malted.dev>
2024-11-18 20:02:38 -0500

    Fix node types import

--- src/lib/components/flow/types.js
+++ src/lib/components/flow/types.js
@@ -1,13 +1,27 @@
-const nodes = Object.values(import.meta.glob("./nodes/*.svelte", { eager: true }));
+import AddNode from "./nodes/add.svelte"
+import CommentNode from "./nodes/comment.svelte"
+import DivideNode from "./nodes/divide.svelte"
+import MultiplyNode from "./nodes/multiply.svelte"
+import OneMinusNode from "./nodes/one-minus.svelte"
+import OutputNode from "./nodes/output.svelte"
+import SineNode from "./nodes/sine.svelte"
+import SplitNode from "./nodes/split.svelte"
+import SubtractNode from "./nodes/subtract.svelte"
+import TimeNode from "./nodes/time.svelte"
+import UvNode from "./nodes/uv.svelte"
+import VecNode from "./nodes/vec.svelte"
 
-export const nodeTypes = Object.fromEntries(
-	nodes.map((n) => {
-		const name = n.default[Object.getOwnPropertySymbols(n.default)[0]]
-			.split("/")
-			.at(-1)
-			.split(".svelte")[0] // Kebab case name to camel case name
-			.replace(/-([a-z])/g, (match, letter) => letter.toUpperCase()); // Ditto
-
-		return [name, n.default];
-	})
-);
+export const nodeTypes = {
+  add: AddNode,
+  comment: CommentNode,
+  divide: DivideNode,
+  multiply: MultiplyNode,
+  oneMinus: OneMinusNode,
+  output: OutputNode,
+  sine: SineNode,
+  split: SplitNode,
+  subtract: SubtractNode,
+  time: TimeNode,
+  uv: UvNode,
+  vec: VecNode
+}
